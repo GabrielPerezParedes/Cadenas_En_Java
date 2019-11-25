ef5ee056ed9e0d66e8a5cceb9293ac8d20bbe82e
Gabriel Perez Paredes <gabogabygpp16@gmail.com>
2019-11-25 12:22:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cadenas_En_Java
---
+++ src/view/Print.java
@@ -0,0 +1,17 @@
+package view;
+
+public class Print {
+
+	public static void main(String[] args) {
+
+		String palabra = "hola que tal";
+		
+		System.out.println("La palabra es :" +palabra);
+        
+        /*Las cadenas son realmente los String que nosotros usamos para definir un conjunto de palabras
+        *o una horacion designada por String nombreDelString = "Las comillas son necesarias";
+        *
+        */
+	}
+
+}
